f642fe31c9a1e723ccd35c9e5cc5e3f8f96713f3
Nienke van Unen <8579642+nienkevanunen@users.noreply.github.com>
2023-02-03 09:59:57 +0100

    verwijder nagelstijlist

--- index.php
+++ index.php
@@ -251,6 +251,7 @@
             </ul>
 					</div>
         </div>
+	      <!--
         <div class="row justify-content-center">
         <div class="col-md-10">
           <h3 class="mt-4">Nagelbehandeling <mark class="text-muted">Nieuw!</mark></h3>
@@ -258,7 +259,7 @@
           Vanaf 3 oktober 2022 kunt u ook een afspraak maken bij <b>Mel's Luxury Nails</b>. <br>
           Neem contact op (+31 6 28583852) of kom langs!
         </div>
-      </div>
+      </div>-->
     </section>
 
     <!--==========================
